8b23869a7809523c50407ad819fb9bcaffa6c9bf
rmbq <davide.rombola@gmail.com>
2013-02-28 20:55:18 +0100

    higher overvolt values

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -377,12 +377,12 @@ static struct l2_level l2_freq_tbl_8960_kraitv2[] = {
 #define OV_GAP 0
 #else /*OVERVOLT*/
 #ifdef CONFIG_OVERVOLT_SOFT
-#define OV_GAP 25000
+#define OV_GAP 50000
 #else /*OVERVOLT_SOFT*/
 #ifdef CONFIG_OVERVOLT_MEDIUM
-#define OV_GAP 50000
+#define OV_GAP 100000
 #else /*OVERVOLT_MEDIUM*/
-#define OV_GAP 75000
+#define OV_GAP 125000
 #endif/*OVERVOLT_MEDIUM*/
 #endif /*OVERVOLT_SOFT*/
 #endif /*OVERVOLT*/
