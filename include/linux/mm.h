e55b3d39e0235a5fe33adedde3f9cedeac89e5f6
MikeC84 <mikec4891@gmail.com>
2013-03-05 10:38:08 -0700

    mm: increase min / max readahead buffers
    Conflicts:
    	include/linux/mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1472,8 +1472,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	1024	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	2048	/* kbytes */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
@@ -1692,4 +1692,3 @@ extern void copy_user_huge_page(struct page *dst, struct page *src,
 
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
-
